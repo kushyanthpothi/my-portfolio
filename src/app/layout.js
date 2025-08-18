189234ef0ecf840e9a9af7cd8abfa1eab5e0cc2a
kushyanthpothi <kushyanthpothineni2003@gmail.com>
2025-08-18 10:26:27 +0530

    FIxed the social media issue in the main home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -104,6 +104,9 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         
+        {/* Font Awesome */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        
         {/* Comprehensive Structured Data for Main Site + Project Sublinks */}
         <script
           type="application/ld+json"
